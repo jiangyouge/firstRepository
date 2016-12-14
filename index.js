ad88e13de3d6bc7ced9355b0a84fc010891b1634
ceshi <ceshi@.cn>
2016-12-14 22:27:20 +0800

    2、在dev分支上添加内容

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 * @Author: lijinquan
 * @Date:   2016-12-14 22:00:16
 * @Last Modified by:   lijinquan
-* @Last Modified time: 2016-12-14 22:22:07
+* @Last Modified time: 2016-12-14 22:25:37
 */
 
 'use strict';
-console.log("1、添加主分支")；
+console.log("1、添加主分支");
+
+console.log("这是在dev分支上添加的内容");
+
