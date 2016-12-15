429f556a4598f54f38e4162a11e5e01f703bc9c8
ceshi <ceshi@.cn>
2016-12-15 22:46:26 +0800

    不知是什么的更新

--- index.js
+++ index.js
@@ -2,11 +2,20 @@
 * @Author: lijinquan
 * @Date:   2016-12-14 22:00:16
 * @Last Modified by:   lijinquan
-* @Last Modified time: 2016-12-14 22:25:37
+* @Last Modified time: 2016-12-15 10:31:04
 */
 
 'use strict';
-console.log("1、添加主分支");
+// console.log("1、添加主分支");
 
-console.log("这是在dev分支上添加的内容");
+// console.log("这是在dev分支上添加的内容");
+(function(angular){
+	angular.module('C3App',[]).controller('C3Controller',['$scope',function($scope){
+		$scope.isLoading=false;
 
+
+
+
+		$scope.isLoading=true;
+	}]);
+})(angular)
